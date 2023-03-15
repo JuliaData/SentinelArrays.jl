cfe75ee3e2eee80bbd322600367822a00d0aa696
KeithWM <keith@myerscough.nl>
2023-03-15 20:27:07 +0100

    Switch to using retest for faster development. Undone autoformat in rebase.

---
+++ test/SentinelArrayTests.jl
@@ -0,0 +1,11 @@
+module SentinelArrayTests
+using ReTest
+using Random, SparseArrays
+
+using SentinelArrays
+
+include("sentinelarrays.jl")
+include("missingvector.jl")
+include("chainedvector.jl")
+
+end
